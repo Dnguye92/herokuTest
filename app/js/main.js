e9ffe87a6408e37da2e6768556f7a7307063d0af
Dylan Nguyen <Dnguye92@yahoo.com>
2017-09-22 16:29:59 -0700

    adding conditions to check for errors

--- app/js/main.js
+++ app/js/main.js
@@ -18,80 +18,85 @@ function createInputs() {
 	}
 	
 	console.log(parseInt(catAmount.value));
-	for(var i = 0; i < parseInt(catAmount.value); i++) {
-		var genInputContainer = document.createElement('div');
-		genInputContainer.className = 'inputContainer';
+	if(typeof parseInt(catAmount.value) === 'number') {
+		for(var i = 0; i < parseInt(catAmount.value); i++) {
+			var genInputContainer = document.createElement('div');
+			genInputContainer.className = 'inputContainer';
 
-		console.log('The current count is ' + i);
+			console.log('The current count is ' + i);
 
-		if(currentCat === 'level_1_cat') {
+			if(currentCat === 'level_1_cat') {
 
-			var level_1_label = document.createElement('h3');
-			level_1_label.textContent = 'Title for level-1 category #' + (i + 1);
+				var level_1_label = document.createElement('h3');
+				level_1_label.textContent = 'Title for level-1 category #' + (i + 1);
 
-			var level_1_name = document.createElement('input');
-			level_1_name.className = 'form-control level_1 level_1-name catName';
-			level_1_name.setAttribute('placeholder', 'Level-1 name');
+				var level_1_name = document.createElement('input');
+				level_1_name.className = 'form-control level_1 level_1-name catName';
+				level_1_name.setAttribute('placeholder', 'Level-1 name');
 
-			var level_1_url_label = document.createElement('h4');
-			level_1_url_label.textContent = 'category url level-1 category #' + (i + 1);
+				var level_1_url_label = document.createElement('h4');
+				level_1_url_label.textContent = 'category url level-1 category #' + (i + 1);
 
-			var level_1_url = document.createElement('input');
-			level_1_url.className = 'form-control level_1 level_2-url catUrl';
-			level_1_url.setAttribute('placeholder', 'Level-1 url');
+				var level_1_url = document.createElement('input');
+				level_1_url.className = 'form-control level_1 level_2-url catUrl';
+				level_1_url.setAttribute('placeholder', 'Level-1 url');
 
-			genInputContainer.append(level_1_label);
-			genInputContainer.append(level_1_name);
-			genInputContainer.append(level_1_url_label);
-			genInputContainer.append(level_1_url);
-			genInputs.append(genInputContainer);
+				genInputContainer.append(level_1_label);
+				genInputContainer.append(level_1_name);
+				genInputContainer.append(level_1_url_label);
+				genInputContainer.append(level_1_url);
+				genInputs.append(genInputContainer);
 
-		} else if(currentCat === 'level_2_cat') {
+			} else if(currentCat === 'level_2_cat') {
 
-			var level_2_label = document.createElement('h3');
-			level_2_label.textContent = 'Example title for level-2 category #' + (i + 1);
+				var level_2_label = document.createElement('h3');
+				level_2_label.textContent = 'Example title for level-2 category #' + (i + 1);
 
-			var level_2_name = document.createElement('input');
-			level_2_name.className = 'form-control level_2 level_2-name catName';
-			level_2_name.setAttribute('placeholder', 'Level-2 name');
+				var level_2_name = document.createElement('input');
+				level_2_name.className = 'form-control level_2 level_2-name catName';
+				level_2_name.setAttribute('placeholder', 'Level-2 name');
 
-			var level_2_url_label = document.createElement('h4');
-			level_2_url_label.textContent = 'Example category url level-2 category #' + (i + 1);
+				var level_2_url_label = document.createElement('h4');
+				level_2_url_label.textContent = 'Example category url level-2 category #' + (i + 1);
 
-			var level_2_url = document.createElement('input');
-			level_2_url.className = 'form-control level_2 level_2-url catUrl';
-			level_2_url.setAttribute('placeholder', 'Level-2 url');
+				var level_2_url = document.createElement('input');
+				level_2_url.className = 'form-control level_2 level_2-url catUrl';
+				level_2_url.setAttribute('placeholder', 'Level-2 url');
 
-			genInputContainer.append(level_2_label);
-			genInputContainer.append(level_2_name);
-			genInputContainer.append(level_2_url_label);
-			genInputContainer.append(level_2_url);
-			genInputs.append(genInputContainer);
+				genInputContainer.append(level_2_label);
+				genInputContainer.append(level_2_name);
+				genInputContainer.append(level_2_url_label);
+				genInputContainer.append(level_2_url);
+				genInputs.append(genInputContainer);
 
-		} else if(currentCat === 'level_3_cat') {
+			} else if(currentCat === 'level_3_cat') {
 
-			var level_3_label = document.createElement('h3');
-			level_3_label.textContent = 'TItle for level-3 category #' + (i + 1);
+				var level_3_label = document.createElement('h3');
+				level_3_label.textContent = 'TItle for level-3 category #' + (i + 1);
 
-			var level_3_name = document.createElement('input');
-			level_3_name.className = 'form-control level_3 level_3-name catName';
-			level_3_name.setAttribute('placeholder', 'Level-3 name');
+				var level_3_name = document.createElement('input');
+				level_3_name.className = 'form-control level_3 level_3-name catName';
+				level_3_name.setAttribute('placeholder', 'Level-3 name');
 
-			var level_3_url_label = document.createElement('h4');
-			level_3_url_label.textContent = 'category url level-3 category #' + (i + 1);
+				var level_3_url_label = document.createElement('h4');
+				level_3_url_label.textContent = 'category url level-3 category #' + (i + 1);
 
-			var level_3_url = document.createElement('input');
-			level_3_url.className = 'form-control level_3 level_3-url catUrl';
-			level_3_url.setAttribute('placeholder', 'Level-3 url');
+				var level_3_url = document.createElement('input');
+				level_3_url.className = 'form-control level_3 level_3-url catUrl';
+				level_3_url.setAttribute('placeholder', 'Level-3 url');
 
-			genInputContainer.append(level_3_label);
-			genInputContainer.append(level_3_name);
-			genInputContainer.append(level_3_url_label);
-			genInputContainer.append(level_3_url);
-			genInputs.append(genInputContainer);
+				genInputContainer.append(level_3_label);
+				genInputContainer.append(level_3_name);
+				genInputContainer.append(level_3_url_label);
+				genInputContainer.append(level_3_url);
+				genInputs.append(genInputContainer);
+			}
+			genBtn.setAttribute('disabled', 'true');
 		}
+	} else {
+		alert('This is not a valid number');
 	}
-	genBtn.setAttribute('disabled', 'true');
+	
 }
 
 function parseData() {
